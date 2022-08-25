279884280bf2e56f29dc6b6bf78e80911512a0e7
= <=>
2022-08-25 22:44:48 +0200

    DSAD

--- src/App.js
+++ src/App.js
@@ -1,21 +1,94 @@
-import {useState,useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
+import Tesseract from 'tesseract.js';
 
-function App() {
+import "./App.css";
 
-  const [count,setCount ]=useState(0)
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [image, setImage] = React.useState('');
+  const [text, setText] = React.useState('');
+  const [progress, setProgress] = React.useState(0);
+  const [resetButton,setResetButton]=useState(false)
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+    Tesseract.recognize(image, 'spa', {
+      logger: (m) => {
+        console.log(m);
+        if (m.status === 'recognizing text') {
+          setProgress(parseInt(m.progress * 100));
+        }
+      },
+    })
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((result) => {
+        console.log(result.data);
+        setResetButton(!resetButton)
+        setText(result.data.text);
+        setIsLoading(false);
+      });
+  };
+
+  const reload = ()=>{
+    window.location.reload();
+  }
   return (
-    <div className="App">
-      
+    <div className="container" style={{ height: '100vh' }}>
+      <div className="row h-100">
+        <div className="col-md-5 mx-auto h-100 d-flex flex-column justify-content-center">
+          {!isLoading && (
+            <h1 className="text-center py-5 mc-5">Image To Text</h1>
 
-<button onClick={()=>setCount(count+1)}>+</button>
-<h1>{count}</h1>
-<button onClick={()=>setCount(count-1)}>-</button>
+          )}
+                      {resetButton?<button  
+                      onClick={()=> reload()}
+                                      className="btn btn-primary mt-5"
+                      >Regresar</button>: ""}
 
+          {isLoading && (
+            <>
+              <progress className="form-control" value={progress} max="100">
+                {progress}%{' '}
+              </progress>{' '}
+              <p className="text-center py-0 my-0">Converting:- {progress} %</p>
+            </>
+          )}
+          {!isLoading && !text && (
+            <>
+              <input
+                type="file"
+                onChange={(e) =>
+                  setImage(URL.createObjectURL(e.target.files[0]))
+                }
+                className="form-control mt-5 mb-2"
+              />
+              <input
+                type="button"
+                onClick={handleSubmit}
+                className="btn btn-primary mt-5"
+                value="Convert"
+              />
+            </>
+          )}
+         
+          {!isLoading && text && (
+            <>
+              <textarea
+                className="form-control w-100 mt-5"
+                rows="30"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              ></textarea>
+            </>
+          )}
+          
+        </div>
 
+      </div>
     </div>
   );
-}
+};
 
 export default App;
